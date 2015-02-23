df26b3e5f3c6e6aa8048528401103ba36d4856a6
Sam Dodrill <xena@yolo-swag.com>
2015-02-23 10:30:31 -0800

    Don't expose debug at the router level
    Moves the check for debug code to the point where the routes table is
    being built. This allows the router to intelligently choose when to add
    debug code or not.

--- routes.php
+++ routes.php
@@ -2,7 +2,10 @@
 use Icebreath\API\Route;
 
 Route::add_route('/', "BaseController");
-Route::add_route('/debug', "DebugController");
 Route::add_restful_route('/shoutcast', 'shoutcast');
 Route::add_restful_route('/icecast', 'icecast');
 Route::add_restful_route('/shoutirc', 'shoutirc');
+
+if(ICEBREATH_DEBUG) {
+	Route::add_route('/debug', "DebugController");
+}
